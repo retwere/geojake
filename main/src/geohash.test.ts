ce570b542042f13775439e98dc08518437bb53e5
Jake Fowler <retwere@gmail.com>
2021-03-01 16:56:21 -0800

    add more tests to geohash.test.ts. Add coverage to test output

--- main/src/geohash.test.ts
+++ main/src/geohash.test.ts
@@ -1,3 +1,4 @@
+import * as g from "./geometry";
 import * as h from "./geohash";
 
 describe("bitsToHash", () => {
@@ -73,5 +74,43 @@ describe("hashToBits", () => {
       0,
       0,
     ]);
+  });
+});
+
+describe("GeohashNode", () => {
+  describe("isLeaf", () => {
+    it("determines whether a node is a leaf", () => {
+      const root = new h.GeohashNode();
+      const child = root.makeChild(0);
+      expect(root.isLeaf()).toBeFalsy();
+      expect(child.isLeaf()).toBeTruthy();
+    });
+  });
+
+  describe("isRoot", () => {
+    it("determines whether a node is the root", () => {
+      const root = new h.GeohashNode();
+      const child = root.makeChild(0);
+      expect(root.isRoot()).toBeTruthy();
+      expect(child.isRoot()).toBeFalsy();
+    });
+  });
+
+  describe("subHashBox", () => {
+    describe("when evenBit is true", () => {
+      it("splits the node's box in half by longitude", () => {
+        const node = new h.GeohashNode(new g.Box([0, 0, 10, 50]), true);
+        expect(node.subHashBox(0)).toStrictEqual(new g.Box([0, 0, 10, 25]));
+        expect(node.subHashBox(1)).toStrictEqual(new g.Box([0, 25, 10, 50]));
+      });
+    });
+
+    describe("when evenBit is false", () => {
+      it("splits the node's box in half by latitude", () => {
+        const node = new h.GeohashNode(new g.Box([0, 0, 10, 50]), false);
+        expect(node.subHashBox(0)).toStrictEqual(new g.Box([0, 0, 5, 50]));
+        expect(node.subHashBox(1)).toStrictEqual(new g.Box([5, 0, 10, 50]));
+      });
+    });
   });
 });
