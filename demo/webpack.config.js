809f6510e04eb35446aa863a9ae9b28dbe40bb9b
Jake Fowler <retwere@gmail.com>
2021-03-10 22:37:08 -0800

    HTML template

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,9 +1,11 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "app"),
+    path: path.resolve(__dirname, "app/lib"),
+    publicPath: "/lib/",
     filename: "bundle.js"
   },
   module: {
@@ -12,5 +14,15 @@ module.exports = {
       loader: "awesome-typescript-loader"
     }]
   },
-  plugins: []
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "app/index.html",
+      template: "src/index.ejs"
+    })
+  ],
+  devServer: {
+    index: "app/index.html",
+    publicPath: "/lib/",
+    contentBase: path.resolve(__dirname, "app"),
+  },
 };
